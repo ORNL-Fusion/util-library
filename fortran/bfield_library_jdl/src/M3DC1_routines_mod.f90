2662b646bae390a2d28f4d0370a374d4f18c0748
Jeremy Lore <lorejd@ornl.gov>
2016-04-28 16:08:09 -0400

    Generalized m3dc1 routines to use multiple coils

--- fortran/bfield_library_jdl/src/M3DC1_routines_mod.f90
+++ fortran/bfield_library_jdl/src/M3DC1_routines_mod.f90
@@ -9,19 +9,21 @@
 !     Subroutine close_m3dc1_fields
 !
 !    m3dc1_itime  : time slice to read (0 = vacuum only, 1 = with response)
-!    m3dc1_factor : factor by which to multiply perturbed (linear) part of solution
+!    m3dc1_factors : factor by which to multiply perturbed (linear) part of solution
 !    m3dc1_toroidal_on_err : On error flag from m3dc1 routine, return B=Bt=1
 !    m3dc1_field_type : return total field (0) or perturbed part only (1)
 !-----------------------------------------------------------------------------
 Module M3DC1_routines_mod
 Use kind_mod, Only: real64, int32
 Implicit None
+Integer(int32), Parameter  :: num_sets_max = 10
 Integer(int32), Save :: m3dc1_itime = -1
 Integer(int32), Save :: m3dc1_field_type = -1
-Real(real64), Save   :: m3dc1_factor = 0.d0
+Real(real64), Save   :: m3dc1_factors(num_sets_max) = 0.d0
 Logical :: m3dc1_toroidal_on_err = .false.
 
-Integer(int32), Private, Save :: isrc, imag, imag_2d, ivec_2d
+Integer, Private, Save :: num_sets
+Integer(int32), Private, Save :: isrc(num_sets_max), imag(num_sets_max), imag_2d, ivec_2d
 Real(real64), Private, Save   :: psi_axis, psi_lcfs
 
 Contains
@@ -29,7 +31,7 @@ Contains
 !-----------------------------------------------------------------------------
 !+ 
 !-----------------------------------------------------------------------------
-Subroutine prepare_m3dc1_fields(filename)
+Subroutine prepare_m3dc1_fields(filenames)
 !
 ! Description:
 ! Input:
@@ -46,61 +48,66 @@ Subroutine prepare_m3dc1_fields(filename)
 Use kind_mod, Only: int32
 Use fusion_io
 Implicit None
-Character(len=120), Intent(In) :: filename
-Integer(int32) :: ierr, ipsi_axis, ipsi_lcfs
+Character(len=*), Intent(In) :: filenames(:)
+Integer(int32) :: ierr, ipsi_axis, ipsi_lcfs, iset
 !- End of header -------------------------------------------------------------
 
 ! Open M3D-C1 source
 ! ~~~~~~~~~~~~~~~~~~
-Write(*,'(a,a)') ' Reading ', Trim(filename)
-Call fio_open_source_f(FIO_M3DC1_SOURCE, Trim(filename), isrc, ierr)
-If (ierr .ne. 0) Then
-  Write(*,*) 'Error opening m3dc1 source file, exiting from prepare_m3dc1_fields'
-  Stop
-Endif
+num_sets = size(filenames)
+Write(*,*) 'In M3DC1 fields, reading ',num_sets,' files'
+Do iset = 1,num_sets
+  Write(*,'(a,a)') ' Reading ', Trim(filenames(iset))
+  Call fio_open_source_f(FIO_M3DC1_SOURCE, Trim(filenames(iset)), isrc(iset), ierr)
+  If (ierr .ne. 0) Then
+    Write(*,*) 'Error opening m3dc1 source file, exiting from prepare_m3dc1_fields'
+    Stop
+  Endif
+  
+  ! Get and set options
+  ! ~~~~~~~~~~~
+  Call fio_get_options_f(isrc(iset), ierr)
+  If ( m3dc1_itime == -1 ) Then
+    Write(*,*) 'm3dc1_itime has not been set!',m3dc1_itime
+    Stop
+  Endif
+  Call fio_set_int_option_f(FIO_TIMESLICE, m3dc1_itime, ierr)
 
-! Get and set options
-! ~~~~~~~~~~~
-Call fio_get_options_f(isrc, ierr)
-If ( m3dc1_itime == -1 ) Then
-  Write(*,*) 'm3dc1_itime has not been set!',m3dc1_itime
-  Stop
-Endif
-Call fio_set_int_option_f(FIO_TIMESLICE, m3dc1_itime, ierr)
+  if (iset == 1) Then
+    ! read 2D equilibrium field data
+    ! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Call fio_set_int_option_f(FIO_PART, FIO_EQUILIBRIUM_ONLY, ierr)
+    Call fio_get_field_f(isrc(iset), FIO_MAGNETIC_FIELD,   imag_2d, ierr)  
+    Call fio_get_field_f(isrc(iset), FIO_VECTOR_POTENTIAL, ivec_2d, ierr)
+    Call fio_get_series_f(isrc(iset), FIO_MAGAXIS_PSI, ipsi_axis, ierr)
+    Call fio_get_series_f(isrc(iset), FIO_LCFS_PSI, ipsi_lcfs, ierr)
 
-! read 2D equilibrium field data
-! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Call fio_set_int_option_f(FIO_PART, FIO_EQUILIBRIUM_ONLY, ierr)
-Call fio_get_field_f(isrc, FIO_MAGNETIC_FIELD,   imag_2d, ierr)  
-Call fio_get_field_f(isrc, FIO_VECTOR_POTENTIAL, ivec_2d, ierr)
-Call fio_get_series_f(isrc, FIO_MAGAXIS_PSI, ipsi_axis, ierr)
-Call fio_get_series_f(isrc, FIO_LCFS_PSI, ipsi_lcfs, ierr)
+    Call fio_eval_series_f(ipsi_axis, 0._real64, psi_axis, ierr)
+    Call fio_eval_series_f(ipsi_lcfs, 0._real64, psi_lcfs, ierr)
+    Write(*,*) 'Psi at magnetic axis: ', psi_axis
+    Write(*,*) 'Psi at lcfs: ', psi_lcfs
 
-Call fio_eval_series_f(ipsi_axis, 0._real64, psi_axis, ierr)
-Call fio_eval_series_f(ipsi_lcfs, 0._real64, psi_lcfs, ierr)
-Write(*,*) 'Psi at magnetic axis: ', psi_axis
-Write(*,*) 'Psi at lcfs: ', psi_lcfs
-
-Call fio_close_series_f(ipsi_axis, ierr)
-Call fio_close_series_f(ipsi_lcfs, ierr)
+    Call fio_close_series_f(ipsi_axis, ierr)
+    Call fio_close_series_f(ipsi_lcfs, ierr)
+  Endif
 
 
-! read 3D field data
-! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-If (m3dc1_field_type .eq. 0) Then
-  Write(*,*) 'M3DC1 returning total field'
-  Call fio_set_int_option_f(FIO_PART, FIO_TOTAL, ierr) 
-Elseif (m3dc1_field_type .eq. 1) Then
-  Write(*,*) 'M3DC1 returning perturbed field'
-  Call fio_set_int_option_f(FIO_PART, FIO_PERTURBED_ONLY, ierr)
-Else
-  Write(*,*) 'Bad value for m3dc1_field_type:',m3dc1_field_type
-  Stop
-Endif
-Write(*,*) 'M3DC1 factor:', m3dc1_factor
-Call fio_set_real_option_f(FIO_LINEAR_SCALE, m3dc1_factor, ierr)
-Call fio_get_field_f(isrc, FIO_MAGNETIC_FIELD, imag, ierr)
-
+  ! read 3D field data
+  ! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  If (m3dc1_field_type .eq. 0) Then
+    Write(*,*) 'M3DC1 returning total field'
+    Call fio_set_int_option_f(FIO_PART, FIO_TOTAL, ierr) 
+  Elseif (m3dc1_field_type .eq. 1) Then
+    Write(*,*) 'M3DC1 returning perturbed field'
+    Call fio_set_int_option_f(FIO_PART, FIO_PERTURBED_ONLY, ierr)
+  Else
+    Write(*,*) 'Bad value for m3dc1_field_type:',m3dc1_field_type
+    Stop
+  Endif
+  Write(*,*) 'M3DC1 factor:', m3dc1_factors(iset)
+  Call fio_set_real_option_f(FIO_LINEAR_SCALE, m3dc1_factors(iset), ierr)
+  Call fio_get_field_f(isrc(iset), FIO_MAGNETIC_FIELD, imag(iset), ierr)
+Enddo
 
 End Subroutine prepare_m3dc1_fields
 
@@ -120,26 +127,29 @@ Real(real64), Intent(Out), Dimension(Npts,3) :: Bout
 Integer(int32), Intent(Out) :: ierr
 ! Local variables
 Real(real64) :: x(3), b_tmp(3)
-Integer(int32) :: ierr_b, i
+Integer(int32) :: ierr_b, i, iset
 ierr = 0
 Do i=1,Npts
   x(1) = r(i)
   x(2) = phi(i)
-  x(3) = z(i) 
-  
-  Call fio_eval_field_f(imag, x, b_tmp, ierr_b)  ! b_tmp(R,phi,Z)
+  x(3) = z(i)
 
-  If (ierr_b .ne. 0) Then
-    If (m3dc1_toroidal_on_err) Then
-      b_tmp = 0._real64
-      b_tmp(2) = 1._real64 
-    Else
-      ierr = 1
+  b_tmp = 0._Real64
+  Do iset = 1,num_sets
+    Call fio_eval_field_f(imag(iset), x, b_tmp, ierr_b)  ! b_tmp(R,phi,Z)
+    
+    If (ierr_b .ne. 0) Then
+      If (m3dc1_toroidal_on_err) Then
+        b_tmp = 0._real64
+        b_tmp(2) = 1._real64 
+      Else
+        ierr = 1
+      Endif
     Endif
-  Endif
-  Bout(i,1) = b_tmp(1)
-  Bout(i,2) = b_tmp(3)
-  Bout(i,3) = b_tmp(2)  
+    Bout(i,1) = Bout(i,1) + b_tmp(1)
+    Bout(i,2) = Bout(i,2) + b_tmp(3)
+    Bout(i,3) = Bout(i,3) + b_tmp(2)
+  Enddo
 Enddo
 
 End Subroutine bfield_m3dc1
@@ -165,7 +175,7 @@ Do i=1,Npts
   x(1) = r(i)
   x(2) = 0._real64
   x(3) = z(i) 
-  
+
   Call fio_eval_field_f(imag_2d, x, b_tmp, ierr_b)  ! b_tmp(R,phi,Z)
 
   If (ierr_b .ne. 0) Then
@@ -253,7 +263,7 @@ Subroutine close_m3dc1_fields
 Use kind_mod, Only: int32
 Use fusion_io
 Implicit None
-Integer(int32) :: ierr
+Integer(int32) :: ierr, iset
 !- End of header -------------------------------------------------------------
 
 ! Close fields and source
@@ -262,10 +272,14 @@ Integer(int32) :: ierr
 !Call fio_close_field_f(ini, ierr)
 !Call fio_close_field_f(ipres, ierr)
 !Call fio_close_field_f(ipe, ierr)
-Call fio_close_field_f(imag, ierr)
-Call fio_close_field_f(imag_2d, ierr)
-Call fio_close_field_f(ivec_2d, ierr)
-Call fio_close_source_f(isrc, ierr)
+Do iset = 1,num_sets
+  Call fio_close_field_f(imag(iset), ierr)
+  If (iset == 1) Then
+    Call fio_close_field_f(imag_2d, ierr)
+    Call fio_close_field_f(ivec_2d, ierr)
+  Endif
+  Call fio_close_source_f(isrc(iset), ierr)
+Enddo
 End Subroutine close_m3dc1_fields
 
 End Module M3DC1_routines_mod
