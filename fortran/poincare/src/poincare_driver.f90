4c0772f6b472b858d2a1e569959aea3bc916bcaf
Jeremy Lore <lorejd@ornl.gov>
2016-08-04 09:55:13 -0400

    added ipec to poincare, and fixed bug in building icoils!

--- fortran/poincare/src/poincare_driver.f90
+++ fortran/poincare/src/poincare_driver.f90
@@ -16,6 +16,7 @@ Use math_geo_module, Only : rlinspace
 Use fieldline_follow_mod, Only: bfield_method, follow_fieldlines_rzphi
 Use util_routines, Only: get_psin_2d
 Use phys_const, Only: pi
+Use ipec_module, Only: open_ipec_fields
 Implicit none
 
 Logical, Parameter :: m3dc1_toroidal_off_grid = .true.
@@ -42,19 +43,22 @@ Character(Len=120) :: &
  rmp_type = 'none',  &
  gfile_name = 'none', &
  rmp_coil_type = 'none', &
- m3dc1_filenames(max_m3dc1_files)
+ m3dc1_filenames(max_m3dc1_files), &
+ ipec_run_path = 'none'
 
 Integer(int32) :: &
  m3dc1_time = -1, &
  num_pts = 2, &
  ntransits = 1, &
  Nsym = 1, &
- m3dc1_nsets = 0
+ m3dc1_nsets = 0, &
+ ipec_field_eval_type = -1
 
 ! Namelist files
 Namelist / settings_nml / gfile_name, rmp_type, rmp_coil_type, m3dc1_filenames, &
      m3dc1_scale_factors, rmp_current, m3dc1_time, phistart_deg, rstart, rend, zstart, zend, &
-     num_pts, ntransits, dphi_line_deg, Nsym, calc_psiN_min, follow_both_ways,m3dc1_nsets
+     num_pts, ntransits, dphi_line_deg, Nsym, calc_psiN_min, follow_both_ways, m3dc1_nsets, &
+     ipec_run_path, ipec_field_eval_type
 
 !---------------------------------------------------------------------------
 !---------------------------------------------------------------------------
@@ -106,7 +110,7 @@ Select Case (rmp_type)
         rmp_ncoil_pts = 12*(2*ntor_pts_coil+1)
         Allocate(rmp_coil(rmp_ncoil_pts,3))
         Allocate(rmp_coil_current(rmp_ncoil_pts))
-        Call build_d3d_ccoils_jl(rmp_current(1:12),ntor_pts_coil,rmp_coil,rmp_coil_current,rmp_ncoil_pts)
+        Call build_d3d_icoils_jl(rmp_current(1:12),ntor_pts_coil,rmp_coil,rmp_coil_current,rmp_ncoil_pts)
       Case Default
         Write(*,*) 'Error!  Unknown rmp_coil_type'
         Write(*,*) 'Current options are:'
@@ -163,6 +167,23 @@ Select Case (rmp_type)
     Write(*,'(a,i0)') '---------> M3DC1 time (0 vacuum, 1 response): ',m3dc1_itime
     Write(*,*) '---------> M3DC1 scale factor: ',m3dc1_factors(1:m3dc1_nsets)
     If (m3dc1_toroidal_on_err) Write(*,'(a)') '---------> M3DC1 fields will be set to B=Bt=1 off grid!'
+  Case ('ipec')
+    Write(*,'(a)') '-----> BFIELD METHOD IS IPEC'
+    Write(*,'(a,i0)') '-----> ipec_field_eval_type is: ',ipec_field_eval_type
+    If (ipec_field_eval_type .eq. 0) Then
+      Write(*,'(a)') '-----> Evaluating IPEC fields as EQUILIBRIUM ONLY!'
+      bfield_method = 7
+    Elseif (ipec_field_eval_type .eq. 1) Then
+      Write(*,'(a)') '-----> Evaluating IPEC fields as EQ + VACUUM!'
+      bfield_method = 8
+    Elseif (ipec_field_eval_type .eq. 2) Then
+      Write(*,'(a)') '-----> Evaluating IPEC fields as EQ + PERT!'
+      bfield_method = 9
+    Else
+      Stop "Did not recognize ipec_field_eval_type"
+    Endif
+    Call readg_g3d(gfile_name)
+    Call open_ipec_fields(ipec_run_path)
   Case Default
     Write(*,*) 'Unknown rmp_type in poincare_driver!'
     Write(*,*) 'Current options are:'
@@ -171,6 +192,7 @@ Select Case (rmp_type)
     Write(*,*) '''g3d+m3dc1'''
     Write(*,*) '''m3dc1_full_field'''
     Write(*,*) '''m3dc1_as'''
+    Write(*,*) '''ipec'''
     Stop      
 End Select
 
