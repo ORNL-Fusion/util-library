746197020abe8bd4ecf772e7911526e833019282
Jeremy Lore <lorejd@ornl.gov>
2019-09-12 09:27:54 -0400

    Update readg to allow for truncated ITER cases

--- matlab/bfield_library_jdl/EFIT_routines/readg_g3d.m
+++ matlab/bfield_library_jdl/EFIT_routines/readg_g3d.m
@@ -5,7 +5,7 @@ if nargin < 2
     force_rewrite = 0;
 end
 
-% Version identifies changes to routine that may require re-creating .mat file 
+% Version identifies changes to routine that may require re-creating .mat file
 version_ = 3;
 % v3: Changed outputs to row vectors and added bicub inverse
 
@@ -18,7 +18,7 @@ fname_mat = [filename,'.mat'];
 if exist(fname_mat,'file') ~= 2 || force_rewrite
     disp([' >>>> Reading gfile ',filename])
 else
-    disp(['>>>> Reading .mat version of gfile ',fname_mat])    
+    disp(['>>>> Reading .mat version of gfile ',fname_mat])
     S = load(fname_mat);
     g = S.g;
     
@@ -37,31 +37,46 @@ if fid == -1
     error(['Could not open file: ',filename])
 end
 
-% 
-line = fgetl(fid); 
-g.ecase = sscanf(line(1:8),'%8s'); 
-sscanf(line(8*6+1:8*6+3),'%4i');
+% Line 1, part 1
+line = fgetl(fid);
+g.ecase = sscanf(line(1:8),'%8s');
+sscanf(line(8*6+1:8*6+3),'%4i');   % time
 g.mw = sscanf(line(53:56),'%4i');  % Number of horizontal R grid points
 g.mh = sscanf(line(57:60),'%4i');  % Number of vertical Z grid points
-%
-line = fscanf(fid,'%f%f%f%f%f',5); 
-g.xdim = line(1); 
-g.zdim = line(2); 
-g.rzero = line(3); 
-g.rgrid1 = line(4); 
-g.zmid = line(5);
-%
-line = fscanf(fid,'%f%f%f%f%f',5); 
-g.rmaxis = line(1); 
-g.zmaxis = line(2); 
-g.ssimag = line(3); 
-g.ssibry = line(4); 
-g.bcentr = line(5);
-%
-line = fscanf(fid,'%f',5); 
-g.cpasma = line(1);
-%
-fscanf(fid,'%f',5);
+if (length(g.mw) ~= 1) || (length(g.mh) ~= 1) % some gfiles seem to have this shifted by a space or two
+    temp = sscanf(line(50:end),'%d %d %d');
+    g.mw = temp(2);
+    g.mh = temp(3);
+end
+
+% Line 2
+line = fgetl(fid);
+data = sscanf(line,'%f%f%f%f%f',5);
+g.xdim = data(1);
+g.zdim = data(2);
+g.rzero = data(3);
+g.rgrid1 = data(4);
+g.zmid = data(5);
+% Line 3
+line = fgetl(fid);
+data = sscanf(line,'%f%f%f%f%f',5);
+g.rmaxis = data(1);
+g.zmaxis = data(2);
+g.ssimag = data(3);
+g.ssibry = data(4);
+g.bcentr = data(5);
+% Line 4
+% Some files have empty lines for 4 and 5
+test = fgetl(fid);
+if ~isempty(test)
+    line = sscanf(test,'%f',5);
+    g.cpasma = line(1);
+end
+% Line 5
+test = fgetl(fid);
+if ~isempty(test)
+    sscanf(test,'%f',5);
+end
 %
 g.fpol = fscanf(fid,'%f',g.mw).';  % Poloidal current function in m-T = RBt
 %
@@ -74,19 +89,25 @@ g.pprime = fscanf(fid,'%f',g.mw).'; % P'(psi) in (Nt/m2)/(Wb/rad)
 g.psirz = fscanf(fid,'%f',[g.mw,g.mh]);  % Poloidal flux in Weber/rad
 %
 g.qpsi = fscanf(fid,'%f',g.mw).';
-%
-line = fscanf(fid,'%i',2); 
-g.nbdry = line(1); 
-g.limitr = line(2);
-%
-g.bdry = fscanf(fid,'%f',[2,g.nbdry]);
-%
-g.lim = fscanf(fid,'%f',[2,g.limitr]);
+if ~isempty(g.qpsi)  % file ends here
+    %
+    line = fscanf(fid,'%i',2);
+    g.nbdry = line(1);
+    g.limitr = line(2);
+    %
+    g.bdry = fscanf(fid,'%f',[2,g.nbdry]);
+    %
+    g.lim = fscanf(fid,'%f',[2,g.limitr]);
+    if max(g.lim(:)) > 100
+        fprintf('Warning: lim seems to be in [cm], converting to [m]\n')
+        g.lim = g.lim./100;
+    end
+end
 fclose(fid);
 
 %
 % Postprocessing
-% 
+%
 g.dR = g.xdim/(g.mw-1);
 g.dZ = g.zdim/(g.mh-1);
 
@@ -102,20 +123,21 @@ for i = 0:g.mh-1
     g.z(1,i+1) = g.zmid - 0.5*g.zdim + g.dZ*i;
 end
 
-g.ip_sign = -sign(g.cpasma);
-
-g.bicub_coeffs = get_psi_bicub_coeffs(g);
-g.bicub_coeffs_inv = get_psi_bicub_coeffs_inv(g);
-g.fpol_coeffs = polyfit(g.pn,g.fpol,7);
-g.xk = dbsnak(g.mw,g.pn,3);
-g.fpol_coeffs_spline = dbsint(g.mw,g.pn,g.fpol,3,g.xk);
-g.filename = filename;
-
-%try to parse filename for shot and time
-[~,f2,f3] = fileparts(filename);
-g.shot = sscanf(f2(2:end),'%d');
-g.gfilename = [f2,f3];
-
+if ~isempty(g.qpsi)  % file ends here
+    g.ip_sign = -sign(g.cpasma);
+    
+    g.bicub_coeffs = get_psi_bicub_coeffs(g);
+    g.bicub_coeffs_inv = get_psi_bicub_coeffs_inv(g);
+    g.fpol_coeffs = polyfit(g.pn,g.fpol,7);
+    g.xk = dbsnak(g.mw,g.pn,3);
+    g.fpol_coeffs_spline = dbsint(g.mw,g.pn,g.fpol,3,g.xk);
+    g.filename = filename;
+    
+    %try to parse filename for shot and time
+    [~,f2,f3] = fileparts(filename);
+    g.shot = sscanf(f2(2:end),'%d');
+    g.gfilename = [f2,f3];
+end
 g.version = version_;
 
 disp('>>> Saving .mat version of gfile')
--- matlab/bfield_library_jdl/EFIT_routines/stress_test_find_xpt.m
+++ matlab/bfield_library_jdl/EFIT_routines/stress_test_find_xpt.m
@@ -5,16 +5,18 @@ clearvars;
 fnames{1} = 'C:\Work\DIII-D\160884\g160884.03400_522';
 fnames{2} = 'C:\Work\DIII-D\164723\g164723.03059_410';
 fnames{3} = 'C:\Work\DIII-D\165274\kinetic\g165274.02120';
-fnames{4} = 'C:\Work\NSTX\g140508.00403';
-fnames{5} = 'C:\Work\NSTX\g135183.00433';
-fnames{6} = 'C:\Work\CMOD\PSI2016\EXP_DATA\g1150625014.01009_983';
-fnames{7} = 'C:\Work\JET\gfiles_corrected\g_p82806_t54.4586';
+fnames{4} = 'C:\Work_archive\gfile_ARCHIVE\NSTX\g140508.00403';
+fnames{5} = 'C:\Work_archive\gfile_ARCHIVE\NSTX\g135183.00433';
+fnames{6} = 'C:\Work_archive\CMOD\PSI2016\EXP_DATA\g1150625014.01009_983';
+fnames{7} = 'C:\Work_archive\JET\gfiles_corrected\g_p82806_t54.4586';
 fnames{8} = 'C:\Work\DIII-D\164723\g164723.03059_C1';
-fnames{9} = 'C:\Work\M3DC1\160884\5000\g160884.05000_m3dc1';
+fnames{9} = 'C:\Users\jjl\Dropbox (ORNL)\PSIscidac\WEST\west_54034_10p2s_mag\west_54034_10p2s_mag.eqdsk';
+fnames{10} = 'C:\Users\jjl\Dropbox (ORNL)\PSIscidac\ITER\400s.eqdsk';
+fnames{11} = 'C:\Users\jjl\Dropbox (ORNL)\PSIscidac\ITER\Baseline2008-li0.70.x4.eqdsk';
 
 for i = 1:length(fnames)
     gfile_name = fnames{i};
     fprintf('%s\n',fnames{i})
-    g = readg_g3d(gfile_name);
+    g = readg_g3d(gfile_name,1);
     plot_gfile(g);
 end
