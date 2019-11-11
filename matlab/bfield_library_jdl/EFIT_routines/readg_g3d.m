c66cc3f933575704061d76f4651f973ff5ee8965
Jeremy Lore <lorejd@ornl.gov>
2019-11-11 09:23:10 -0500

    Add readg_g3d_simple for distribution

--- matlab/bfield_library_jdl/EFIT_routines/readg_g3d.m
+++ matlab/bfield_library_jdl/EFIT_routines/readg_g3d.m
@@ -32,78 +32,7 @@ else
     fprintf(' Found version %i (or no version), expected %i, remaking.\n',g.version,version_)
 end
 
-fid = fopen(filename,'r');
-if fid == -1
-    error(['Could not open file: ',filename])
-end
-
-% Line 1, part 1
-line = fgetl(fid);
-g.ecase = sscanf(line(1:8),'%8s');
-sscanf(line(8*6+1:8*6+3),'%4i');   % time
-g.mw = sscanf(line(53:56),'%4i');  % Number of horizontal R grid points
-g.mh = sscanf(line(57:60),'%4i');  % Number of vertical Z grid points
-if (length(g.mw) ~= 1) || (length(g.mh) ~= 1) % some gfiles seem to have this shifted by a space or two
-    temp = sscanf(line(50:end),'%d %d %d');
-    g.mw = temp(2);
-    g.mh = temp(3);
-end
-
-% Line 2
-line = fgetl(fid);
-data = sscanf(line,'%f%f%f%f%f',5);
-g.xdim = data(1);
-g.zdim = data(2);
-g.rzero = data(3);
-g.rgrid1 = data(4);
-g.zmid = data(5);
-% Line 3
-line = fgetl(fid);
-data = sscanf(line,'%f%f%f%f%f',5);
-g.rmaxis = data(1);
-g.zmaxis = data(2);
-g.ssimag = data(3);
-g.ssibry = data(4);
-g.bcentr = data(5);
-% Line 4
-% Some files have empty lines for 4 and 5
-test = fgetl(fid);
-if ~isempty(test)
-    line = sscanf(test,'%f',5);
-    g.cpasma = line(1);
-end
-% Line 5
-test = fgetl(fid);
-if ~isempty(test)
-    sscanf(test,'%f',5);
-end
-%
-g.fpol = fscanf(fid,'%f',g.mw).';  % Poloidal current function in m-T = RBt
-%
-g.pres = fscanf(fid,'%f',g.mw).';  % Plasma pressure in Nt/m2
-%
-g.ffprim = fscanf(fid,'%f',g.mw).'; % FF'(psi) in (mT)%2/(Wb/rad)
-%
-g.pprime = fscanf(fid,'%f',g.mw).'; % P'(psi) in (Nt/m2)/(Wb/rad)
-%
-g.psirz = fscanf(fid,'%f',[g.mw,g.mh]);  % Poloidal flux in Weber/rad
-%
-g.qpsi = fscanf(fid,'%f',g.mw).';
-if ~isempty(g.qpsi)  % file ends here
-    %
-    line = fscanf(fid,'%i',2);
-    g.nbdry = line(1);
-    g.limitr = line(2);
-    %
-    g.bdry = fscanf(fid,'%f',[2,g.nbdry]);
-    %
-    g.lim = fscanf(fid,'%f',[2,g.limitr]);
-    if max(g.lim(:)) > 100
-        fprintf('Warning: lim seems to be in [cm], converting to [m]\n')
-        g.lim = g.lim./100;
-    end
-end
-fclose(fid);
+g = readg_g3d_simple(filename);
 
 %
 % Postprocessing
@@ -123,7 +52,7 @@ for i = 0:g.mh-1
     g.z(1,i+1) = g.zmid - 0.5*g.zdim + g.dZ*i;
 end
 
-if ~isempty(g.qpsi)  % file ends here
+if ~isempty(g.qpsi)  % Do not postprocess truncated file
     g.ip_sign = -sign(g.cpasma);
     
     g.bicub_coeffs = get_psi_bicub_coeffs(g);
