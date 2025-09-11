e00b5999681bb11fb349442ee4ef11f49520ca09
Jeremy Lore <lorejd@ornl.gov>
2025-09-11 15:42:00 -0400

    Updates
    *Add Rsep for inner midplane
    *New colorflipper colors

--- matlab/bfield_library_jdl/EFIT_routines/calc_Rsep_midplane.m
+++ matlab/bfield_library_jdl/EFIT_routines/calc_Rsep_midplane.m
@@ -1,9 +1,14 @@
-function [Rsep] = calc_Rsep_midplane(g)
+function [Rsep,Rsepi] = calc_Rsep_midplane(g)
 
 
 Ninterp = 100;
 Rinterp = linspace(g.rmaxis,g.r(end-2),Ninterp);
 Zinterp = g.zmaxis*ones(1,Ninterp);
 [psiN_mid,~] = calc_psiN(g,Rinterp,Zinterp);
+Rsep = interp1(psiN_mid,Rinterp,1);  
 
-Rsep = interp1(psiN_mid,Rinterp,1);   
+
+Rinterp2 = linspace(g.rmaxis,g.r(2),Ninterp);
+[psiN_midi,~] = calc_psiN(g,Rinterp2,Zinterp);
+Rsepi = interp1(psiN_midi,Rinterp2,1);  
+
