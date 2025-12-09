be62093bba9270589c502898eaa099286863a7a1
Jeremy Lore <lorejd@ornl.gov>
2025-12-09 14:10:42 -0500

    Update get_resScale.m

--- matlab/bfield_library_jdl/get_resScale.m
+++ matlab/bfield_library_jdl/get_resScale.m
@@ -1,20 +1,15 @@
-function f = get_resScale(nxPlot,nyPlot,gap,botBuffer,topBuffer,leftBuffer,rightBuffer)
-%f = get_resScale(nxPlot,nyPlot,gap,botBuffer,topBuffer,leftBuffer,rightBuffer)
+function f = get_resScale(nxPlot,nyPlot,gap,botBuffer,topBuffer,leftBuffer,rightBuffer,xgap)
+%f = get_resScale(nxPlot,nyPlot,gap,botBuffer,topBuffer,leftBuffer,rightBuffer,xgap)
 
 resRaw = get(0,'ScreenSize');
 xScale = resRaw(3)/3440;
 yScale = resRaw(4)/1440;
 f.resScale = [xScale yScale 1 1];
 
-
 if nargin == 0
     f = f.resScale;
     return 
 end
-% 
-% if nxPlot ~= 1
-%     error('Update this for nx > 1')
-% end
 
 if nargin < 3
     gap = 0.04;
@@ -31,24 +26,20 @@ end
 if nargin < 7
     rightBuffer = 0.05;
 end
+if nargin < 8
+    xgap = gap;     % default: same gap for rows and columns
+end
 
-
-
-% dx = 1 - leftBuffer - rightBuffer;
 yMin = botBuffer;
 yMax = 1 - topBuffer;
 xMin = leftBuffer;
 xMax = 1 - rightBuffer;
 
 dy = (yMax - yMin - gap*(nyPlot - 1))/nyPlot;
-dx = (xMax - xMin - gap*(nxPlot - 1))/nxPlot;
-
+dx = (xMax - xMin - xgap*(nxPlot - 1))/nxPlot;
 
 for iy = 1:nyPlot
     for ix = 1:nxPlot
-        % iPlot = iy + (ix-1)*nyPlot;
-        % iPlot = ix + (iy-1)*nxPlot;
-        f.p(ix,iy,:) = [(dx+gap)*(ix-1) + xMin,(dy+gap)*(nyPlot-iy) + yMin,dx,dy];
+        f.p(ix,iy,:) = [(dx+xgap)*(ix-1) + xMin, (dy+gap)*(nyPlot-iy) + yMin, dx, dy];
     end
 end
-
